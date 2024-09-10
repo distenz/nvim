278355c895b3eef059c34557b40a7e5c67126b06
Nicolás Baquero <88566759+Cheveniko@users.noreply.github.com>
2024-09-10 15:27:24 -0500

    feat: update references of tsserver to ts_ls (#1131)

--- init.lua
+++ init.lua
@@ -584,8 +584,8 @@ require('lazy').setup({
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        -- ts_ls = {},
         --
 
         lua_ls = {
@@ -626,13 +626,7 @@ require('lazy').setup({
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
-
-            -- https://github.com/neovim/nvim-lspconfig/pull/3232
-            if server_name == 'tsserver' then
-              server_name = 'ts_ls'
-            end
-
+            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
