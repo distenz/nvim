325f1403f2aab500c1e2986d0ee1acb52bf041e9
Tibor Stana <18554509+distenz@users.noreply.github.com>
2024-09-14 16:36:45 +0200

    feat(keymaps): migrate custom generic keymaps

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -15,3 +15,27 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- [[ Custom Keymaps ]]
+
+-- open netrw
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- move selected lines
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- iterate quickfix list items
+vim.keymap.set('n', '<C-j>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<C-k>', '<cmd>cprev<CR>zz')
+
+vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+
+-- do not overwrite register with the deleted text on paste because we redirect
+-- it to the black hole register
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+-- move half page with recentering
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
